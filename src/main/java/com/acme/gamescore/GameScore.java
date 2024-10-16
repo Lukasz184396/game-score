4e9737347b271d35b3c0fcdb18f3e23e7661b4d0
Łukasz Zawada <zawadalukasz88@gmail.com>
2024-10-16 23:04:39 +0200

    Add git ignore and remove unused imports

--- src/main/java/com/acme/gamescore/GameScore.java
+++ src/main/java/com/acme/gamescore/GameScore.java
@@ -1,7 +1,5 @@
 package com.acme.gamescore;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.OptionalInt;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
